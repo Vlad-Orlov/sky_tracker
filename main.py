b3aa8a9d73011b3bbb1291cc97d2c403a68e8f2a
Vlad-Orlov <orlov.vlad.serg@gmail.com>
2023-03-17 02:55:42 +0200

    constructed observer object

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import astroplan
+from pytz import timezone
 import requests, json
 from astropy.coordinates import SkyCoord, EarthLocation
 from astroplan import Observer, FixedTarget
-
+import astropy.units as u
+from astropy.units import cds
+cds.enable()
 # Get information about observation location and weather
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+
+location = json.load(open('observer_location.json'))
+observer_location = EarthLocation.from_geodetic(location['Longtitude'], 
+                                                location['Latitude'], 
+                                                location['Elevation'])
+
+API_KEY = "76b21e0bf8ed80be297c0b937f476a65"
+URL = (f"https://api.openweathermap.org/data/2.5/weather?"
+       f"lat={location['Latitude']}&lon={location['Longtitude']}"
+       f"&appid={API_KEY}&units=metric")
+
+print(URL)
 response = requests.get(URL)
 
 if response.status_code == 200:
@@ -18,15 +31,10 @@ if response.status_code == 200:
 
 print(temperature, humidity, pressure)
 
-location = json.load(open('observer_location.json'))
-observer_location = EarthLocation.from_geodetic(location['Longtitude'], 
-                                                location['Latitude'], 
-                                                location['Elevation'])
-
-observer = Observer(name='Subaru Telescope',
+observer = Observer(name='VORLOV Telescope',
                location=observer_location,
-               pressure=0.615 * u.bar,
-               relative_humidity=0.11,
-               temperature=0 * u.deg_C,
-               timezone=timezone('US/Hawaii'),
-               description="Subaru Telescope on Maunakea, Hawaii")
+               pressure=pressure * cds.mmHg,
+               relative_humidity=humidity/100,
+               temperature=temperature * u.deg_C,
+               timezone=timezone('Europe/Kiev'),
+               description="Telescope of Vladyslav Orlov, Kyiv, Ukraine")
